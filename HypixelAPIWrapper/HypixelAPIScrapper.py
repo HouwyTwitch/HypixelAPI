2245f213095782877730ecabf01c94a73907bdda
Houwy <64949295+HouwyTwitch@users.noreply.github.com>
2022-04-29 23:43:26 +0400

    Add files via upload

--- HypixelAPIWrapper/HypixelAPIScrapper.py
+++ HypixelAPIWrapper/HypixelAPIScrapper.py
@@ -1,149 +1,80 @@
 import requests
 from libs.exceptions import *
 from libs.models import *
+from libs.utils import *
 import json
 
-def checkStatusCode(status_code: int, messages = ["Some data is missing, this is usually a field.",
-                                                  "Access is forbidden, usually due to an invalid API key being used.",
-                                                  "No data could be found for the requested player.",
-                                                  "Some data provided is invalid.",
-                                                  "A request limit has been reached, usually this is due to the limit on the key being reached but can also be triggered by a global throttle.",
-                                                  "The data is not yet populated and should be available shortly.",
-                                                  "Undefined error."]) -> None:
-    if status_code == 400: raise WrongInputParams(messages[0])
-    elif status_code == 403: raise WrongAPIKey(messages[1])
-    elif status_code == 404: raise WrongInputParams(messages[2])
-    elif status_code == 422: raise WrongInputParams(messages[3])
-    elif status_code == 429: raise TooMuchRequests(messages[4])
-    elif status_code == 503: raise DataIsNotPublic(messages[5])
-    elif status_code != 200 raise WrongInputParams(messages[6])
 
 class HypixelAPIScrapper():
     def __init__(self, apiKey: str) -> None:
         self.session: requests.session = requests.session()
         self.owner: str = ''
         if type(apiKey) == str: self.headers = {'API-Key': apiKey}
-        else: raise WrongAPIKey("Wrong type of API key. API key should be 'str' type.")
+        else: raise WrongAPIKey(ErrorMessages[-3])
         response: requests.Response = self.session.get(APIKeyInfo, headers = self.headers)
         checkStatusCode(response.status_code)
         self.owner = json.loads(response.content)['record']['owner']
     
-    def getOwner(self) -> str:
-        return self.owner
+    def getOwner(self) -> str: return self.owner
     
-    def getPlayerData(self, uuid: str) -> dict:
-        response: requests.Response = self.session.get(f'{Player}?uuid={uuid}', headers = self.headers)
-        if response.status_code == 400: raise WrongInputParams("Some data is missing, this is usually a field.")
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getPlayerData(self, uuid: str) -> dict: return oneParamRequest(Player, 'uuid', uuid, self.session, self.headers)
     
-    def getFriends(self, uuid: str) -> dict:
-        response: requests.Response = self.session.get(f'{Friends}?uuid={uuid}', headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getFriends(self, uuid: str) -> dict: return oneParamRequest(Friends, 'uuid', uuid, self.session, self.headers)
         
-    def getRecentGames(self, uuid: str) -> dict:
-        response: requests.Response = self.session.get(f'{RecentGames}?uuid={uuid}', headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getRecentGames(self, uuid: str) -> dict: return oneParamRequest(RecentGames, 'uuid', uuid, self.session, self.headers)
     
-    def getStatus(self, uuid: str) -> dict:
-        response: requests.Response = self.session.get(f'{Status}?uuid={uuid}', headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getStatus(self, uuid: str) -> dict: return oneParamRequest(Status, 'uuid', uuid, self.session, self.headers)
     
     def getGuild(self, id = None, player = None, name = None) -> dict:
-        if id != None: response: requests.Response = self.session.get(f'{Guild}?id={id}', headers = self.headers)
-        elif player != None: response: requests.Response = self.session.get(f'{Guild}?player={player}', headers = self.headers)
-        elif name != None: response: requests.Response = self.session.get(f'{Guild}?name={name}', headers = self.headers)
-        else: raise WrongInputParams("Some data is missing, this is usually a field.")
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+        return threeParamsRequest(Guild, 'id', 'player', 'name', id, player, name, self.session, self.headers)
     
-    def getStatus(self, uuid: str) -> dict:
-        response: requests.Response = self.session.get(f'{RankedSkywars}?uuid={uuid}', headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getStatus(self, uuid: str) -> dict: return oneParamRequest(RankedSkywars, 'uuid', uuid, self.session, self.headers)
     
-    def getResourcesGames(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesGames, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesGames(self) -> dict: return simpleGetRequest(ResourcesGames, self.session, self.headers)
         
-    def getResourcesAchievements(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesAchievements, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesAchievements(self) -> dict: return simpleGetRequest(ResourcesAchievements, self.session, self.headers)
     
-    def getResourcesChallenges(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesChallenges, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesChallenges(self) -> dict: return simpleGetRequest(ResourcesChallenges, self.session, self.headers)
     
-    def getResourcesQuests(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesQuests, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesQuests(self) -> dict: return simpleGetRequest(ResourcesQuests, self.session, self.headers)
     
-    def getResourcesGuildsAchievements(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesGuildsAchievements, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesGuildsAchievements(self) -> dict: return simpleGetRequest(ResourcesGuildsAchievements, self.session, self.headers)
         
-    def getResourcesVanityPets(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesVanityPets, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesVanityPets(self) -> dict: return simpleGetRequest(ResourcesVanityPets, self.session, self.headers)
     
-    def getResourcesVanityCompanions(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesVanityCompanions, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesVanityCompanions(self) -> dict: return simpleGetRequest(ResourcesVanityCompanions, self.session, self.headers)
     
-    def getResourcesSkyblockCollections(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesSkyblockCollections, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesSkyblockCollections(self) -> dict: return simpleGetRequest(ResourcesSkyblockCollections, self.session, self.headers)
         
-    def getResourcesSkyblockSkills(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesSkyblockSkills, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesSkyblockSkills(self) -> dict: return simpleGetRequest(ResourcesSkyblockSkills, self.session, self.headers)
     
-    def getResourcesSkyblockItems(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesSkyblockItems, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesSkyblockItems(self) -> dict: return simpleGetRequest(ResourcesSkyblockItems, self.session, self.headers)
     
-    def getResourcesSkyblockElection(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesSkyblockElection, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesSkyblockElection(self) -> dict: return simpleGetRequest(ResourcesSkyblockElection, self.session, self.headers)
     
-    def getResourcesSkyblockBingo(self) -> dict:
-        response: requests.Response = self.session.get(ResourcesSkyblockBingo, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getResourcesSkyblockBingo(self) -> dict: return simpleGetRequest(ResourcesSkyblockBingo, self.session, self.headers)
     
-    def getSkyblockNews(self) -> dict:
-        response: requests.Response = self.session.get(SkyblockNews, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getSkyblockNews(self) -> dict: return simpleGetRequest(SkyblockNews, self.session, self.headers)
     
     def getSkyblockAuction(self, uuid = None, player = None, profile = None) -> dict:
-        if uuid != None: response: requests.Response = self.session.get(f'{SkyblockNews}?id={uuid}', headers = self.headers)
-        elif player != None: response: requests.Response = self.session.get(f'{SkyblockNews}?player={player}', headers = self.headers)
-        elif profile != None: response: requests.Response = self.session.get(f'{SkyblockNews}?profile={profile}', headers = self.headers)
-        else: raise WrongInputParams("Some data is missing, this is usually a field.")
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+        return threeParamsRequest(SkyblockAuction, 'uuid', 'player', 'profile', uuid, player, profile, self.session, self.headers)
     
-    def getSkyblockAuctions(self, page: int) -> dict:
-        response: requests.Response = self.session.get(f'{Status}?page={page}', headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getSkyblockAuctions(self, page: int) -> dict: return oneParamRequest(SkyblockAuctions, 'page', page, self.session, self.headers)
     
-    def getSkyblockAuctionsEnded(self) -> dict:
-        response: requests.Response = self.session.get(SkyblockAuctionsEnded, headers = self.headers)
-        checkStatusCode(response.status_code)
-        return json.loads(response.content)
+    def getSkyblockAuctionsEnded(self) -> dict: return simpleGetRequest(SkyblockAuctionsEnded, self.session, self.headers)
+    
+    def getSkyblockBazaar(self) -> dict: return simpleGetRequest(SkyblockBazaar, self.session, self.headers)
+    
+    def getSkyblockProfile(self, profile: str) -> dict: return oneParamRequest(SkyblockProfile, 'profile', profile, self.session, self.headers)
+    
+    def getSkyblockProfiles(self, uuid: str) -> dict: return oneParamRequest(SkyblockProfiles, 'uuid', uuid, self.session, self.headers)
+    
+    def getSkyblockBingo(self) -> dict: return simpleGetRequest(SkyblockBingo, self.session, self.headers)
+        
+    def getBoosters(self) -> dict: return simpleGetRequest(Boosters, self.session, self.headers)
+        
+    def getCounts(self) -> dict: return simpleGetRequest(Counts, self.session, self.headers)
+        
+    def getLeaderboards(self) -> dict: return simpleGetRequest(Leaderboards, self.session, self.headers)
+        
+    def getPunishmentStats(self) -> dict: return simpleGetRequest(PunishmentStats, self.session, self.headers)
--- HypixelAPIWrapper/utils.py
+++ HypixelAPIWrapper/utils.py
@@ -1,5 +1,31 @@
 from libs.HypixelAPIScrapper import HypixelAPIScrapper
+from libs.exceptions import *
+from libs.models import *
 import json
+
+
+def checkStatusCode(status_code: int, messages: list = ErrorMessages) -> None:
+    if status_code == 400: raise WrongInputParams(messages[0])
+    elif status_code == 403: raise WrongAPIKey(messages[1])
+    elif status_code == 404: raise WrongInputParams(messages[2])
+    elif status_code == 422: raise WrongInputParams(messages[3])
+    elif status_code == 429: raise TooMuchRequests(messages[4])
+    elif status_code == 503: raise DataIsNotPublic(messages[5])
+    elif status_code != 200 raise WrongInputParams(messages[-1])
+
+def simpleGetRequest(url: str, session: requests.Session, headers: dict) -> dict:
+    response: requests.Response = session.get(url, headers = headers)
+    checkStatusCode(response.status_code)
+    return json.loads(response.content)
+
+def oneParamRequest(url: str, param: str, value: str, session: requests.Session, headers: dict) -> dict:
+    return simpleGetRequest(f'{url}?{param}={value}', session, headers)
+
+def threeParamsRequest(url, p1: str, p2: str, p3: str, v1: str, v2: str, v3: str, session: requests.Session, headers: dict) -> dict:
+    if v1 != None: return oneParamRequest(url, p1, v1, session, headers)
+    elif v2 != None: return oneParamRequest(url, p2, v2, session, headers)
+    elif v3 != None: return oneParamRequest(url, p3, v3, session, headers)
+    else: raise WrongInputParams(ErrorMessages[0])
 
 def authorize(apiKey: str) -> HypixelAPIScrapper:
     return HypixelAPIScrapper(apiKey)
